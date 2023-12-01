4a3b0eae0782fed49cd94f8b299cb4b08bf70657
TaehyeonPark <devtaehyeon@gmail.com>
2023-12-01 14:26:20 +0900

    Add: test control

--- control.py
+++ control.py
@@ -5,6 +5,7 @@ CAR_SPEED = 50
 CRANE_SPEED = 40
 PUSH_SPEED = 50
 
+
 class Control(MODI):
     def __init__(self, modi_version: int = 1, conn_type: str = "", verbose: bool = False, port=None, network_uuid: str = "", virtual_modules=None):
         super().__init__(modi_version, conn_type, verbose,
@@ -15,7 +16,7 @@ class Control(MODI):
 
     def car_go_straight(self):
         self._car_move(CAR_SPEED, CAR_SPEED)
-    
+
     def car_go_back(self):
         self._car_move(-CAR_SPEED, -CAR_SPEED)
 
@@ -24,7 +25,7 @@ class Control(MODI):
 
     def crane_pull_up(self):
         self._crane_move(CRANE_SPEED)
-    
+
     def crane_stop(self):
         self._crane_move(0)
 
@@ -33,11 +34,51 @@ class Control(MODI):
         sleep(t)
         self._push_move(0)
 
+        self._push_move(-PUSH_SPEED)
+        sleep(t)
+        self._push_move(0)
+
     def _car_move(self, left_speed, right_speed):
         self.CAR_MOTOR.speed = left_speed, right_speed
 
     def _crane_move(self, speed):
-        self.CRANE_MOTER.speed = speed, speed
+        self.CRANE_MOTER.speed = 0, speed
 
     def _push_move(self, speed):
         self.PUSH_MOTOR.speed = speed, 0
+
+
+def reset(ctl: Control):
+    ctl = Control()
+    ctl.CAR_MOTOR.speed = 0, -40
+    sleep(3)
+    ctl.car_stop()
+
+    ctl._crane_move(-40)
+    sleep(1)
+    ctl.crane_stop()
+
+
+if __name__ == "__main__":
+    ## Scenario 1 ###
+    ctl = Control()
+    # ctl.CAR_MOTOR.speed = 0, 40
+    # sleep(3)
+    # ctl.car_stop()
+
+    # ctl._crane_move(40)
+    # sleep(1)
+    # ctl.crane_stop()
+
+    # ctl.push(1)
+
+    # reset(ctl=ctl)
+    # ctl.CAR_MOTOR.speed = 0, 40
+
+    ctl._crane_move(-40)
+    sleep(1)
+    ctl.crane_stop()
+
+    ctl._car_move(0)
+    ctl._push_move(0)
+    ctl._crane_move(0)
